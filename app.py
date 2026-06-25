327a6b726265adc5126fb6f49d1a215c4d27b43c
saccotecnologie-glitch <sacco.tecnologie@gmail.com>
2026-06-25 19:08:06 +0200

    app.py

--- app.py
+++ app.py
@@ -5866,7 +5866,7 @@ with col_main:
 
     lunghezza_traversa = calcola_traversa(luce_mm, ante)
 
-    # Render vecchio rimosso in V802
+    components.html(disegno_porta_v802(ante, luce_mm, altezza_mm, lunghezza_traversa), height=735)
     st.markdown(f"""
     <div class="measure-total">
     <div><b>MISURA TRAVERSA CALCOLATA</b><br>1 anta = doppio luce + 10 cm<br>2 ante = doppio luce + 20 cm</div>
@@ -6592,7 +6592,7 @@ if profilo in ["SA-TEC", "RIVENDITORE", "GROSSISTA"]:
 
     st.markdown("</div>", unsafe_allow_html=True)
 
-st.caption("Versione V802 - SVG statico SESAMO")
+st.caption("Versione V803 - Immagine porta ripristinata")
 
 st.markdown(f"""
 <div class="footer">
